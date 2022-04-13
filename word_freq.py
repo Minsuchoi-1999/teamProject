d6bd7cb87d7ff6d6a9069adc92d34705aa4721c4
Minsu <jungsi@naver.com>
2022-04-14 00:12:37 +0900

    make count

--- word_freq.py
+++ word_freq.py
@@ -2,13 +2,25 @@
 
 def function(name, num):
     words = []
+    D = {}
     fp = open(name, "r")
     lines = fp.readlines()
+    
     for line in lines:
-        line = line.replace('.',' ').replace(',', ' ').replace('!', ' ').replace('?',' ').replace('-',' ')
+        line = line.replace('.',' ').replace(',', ' ').replace('!', ' ').replace("'",' ').replace('-',' ')
         word = line.split()
         words.extend(word)
-    print(words)
+
+    set(words)
+
+    for word in words:
+        D[word] =0
+        for line in lines:
+            D[word] += line.count(word)
+        
+    Dlist = D.items()
+    for d in Dlist:
+        print(d)
     fp.close
 
 
