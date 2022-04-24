44795ebe7e9c66ffa968e2fbfc9721be169ec613
ImChaeha <qls1479@gmail.com>
2022-04-25 01:24:09 +0900

    modified

--- fibo.py
+++ fibo.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
-def fibo(n):
+def fibo():
+	n=int(input("fibonacci number: "))
 	a,b=0,1
+
 	for i in range(n):
 		print(a, end = ' ')
 		if i == n-1:
 			print("\nF",n,"=", a)
 		a,b=b,a+b 
-if__name=="__main__":
+if__name__=="__main__":
 	print()
