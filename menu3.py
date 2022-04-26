396268bf646d3df3e412736cd0e0094e74b321ff
Minsu <jungsi@naver.com>
2022-04-26 17:34:55 +0900

    modified2

--- menu3.py
+++ menu3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
-print("input the 1st list:");
-num_list1 = list(map(int, input().split()));
-print("input the 2nd list:");
-num_list2 = list(map(int, input().split()));
+num_list1 = list(map(int, input("input the 1st list : ").split()));
+num_list2 = list(map(int, input("input the 2nd list : ").split()));
 
 print("union : ",list(set(num_list1) | set(num_list2)));
 print("intersection : ", list(set(num_list1) & set(num_list2)));
